9661df4ef1db7f8a8334cc6b7eae1cc46483848f
JulienSLuk <s10219568@connect.np.edu.sg>
2022-12-04 02:59:15 +0800

    Add Nav Progress Bar and made Nav Bar Sticky

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
